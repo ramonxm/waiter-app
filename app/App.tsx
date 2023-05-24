7b28672925f4c47e181fa5d87aa28b4d27c90b9e
Ramon Xavier <ramon1234rxm@gmail.com>
2023-05-24 09:59:01 -0300

    feat: adding assets, mocks, svgs and configure fonts

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [isFontsLoaded] =  useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-500': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Bold.otf')
+  });
+
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontFamily: 'GeneralSans-400' }}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
