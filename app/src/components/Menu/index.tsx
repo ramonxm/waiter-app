8c96fc34edca50bcb44572fe8c7f08f9e56316f6
Ramon Xavier <ramon1234rxm@gmail.com>
2023-05-29 10:11:44 -0300

    feat: creating menu component

--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -1,4 +1,36 @@
-export const Menu = () => {
+import { FlatList } from 'react-native';
+import { products } from '../../mocks/products';
+import { Text } from '../Text';
 
-  return <></>;
+import * as S from './styles';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
+
+export const Menu = () => {
+  return (
+    <FlatList
+      data={products}
+      style={{ marginTop: 32 }}
+      ItemSeparatorComponent={S.Separator}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      keyExtractor={(product) => product._id}
+      renderItem={({ item: product }) => (
+        <S.Product>
+          <S.Image source={{ uri: product.imagePath }} />
+          <S.ProductDetails>
+            <Text weight="600">{product.name}</Text>
+            <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text size={14} weight="600">
+              {formatCurrency(product.price)}
+            </Text>
+          </S.ProductDetails>
+          <S.AddToCardButton>
+            <PlusCircle />
+          </S.AddToCardButton>
+        </S.Product>
+      )}
+    />
+  );
 };
