d3d1d9233240805309818f8b95562acefc50f450
Ramon Xavier <ramon1234rxm@gmail.com>
2023-05-29 10:11:13 -0300

    feat: adding actions to category

--- app/src/components/Categories/index.tsx
+++ app/src/components/Categories/index.tsx
@@ -2,8 +2,17 @@ import { FlatList } from 'react-native';
 import { categories } from '../../mocks/categories';
 import { Text } from '../Text';
 import * as S from './styles';
+import { useState } from 'react';
 
 export const Categories = () => {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectCategory(categoryId: string) {
+    setSelectedCategory((prevState) =>
+      prevState === categoryId ? '' : categoryId
+    );
+  }
+
   return (
     <FlatList
       horizontal
@@ -11,16 +20,20 @@ export const Categories = () => {
       data={categories}
       contentContainerStyle={{ paddingRight: 24 }}
       keyExtractor={(category) => category._id}
-      renderItem={({ item }) => (
-        <S.Category>
-          <S.Icon>
-            <Text>{item.icon}</Text>
-          </S.Icon>
-          <Text size={14} weight="600">
-            {item.name}
-          </Text>
-        </S.Category>
-      )}
+      renderItem={({ item }) => {
+        const isSelected = selectedCategory === item._id;
+
+        return (
+          <S.Category onPress={() => handleSelectCategory(item._id)}>
+            <S.Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>{item.icon}</Text>
+            </S.Icon>
+            <Text opacity={isSelected ? 1 : 0.5} size={14} weight="600">
+              {item.name}
+            </Text>
+          </S.Category>
+        );
+      }}
     />
   );
 };
--- app/src/components/Categories/styles.ts
+++ app/src/components/Categories/styles.ts
@@ -1,10 +1,13 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Category = styled.View`
+
+const isAndroid = Platform.OS === 'android';
+
+export const Category = styled.TouchableOpacity`
   align-items: center;
   margin-left: 24px;
 `;
-
 
 export const Icon = styled.View`
   background: #fff;
@@ -14,5 +17,5 @@ export const Icon = styled.View`
   align-items: center;
   justify-content: center;
   margin-bottom: 8px;
-
+  box-shadow: 0px 2px 1px rgba(0, 0, 0, ${isAndroid ? 1 : 0.1});
 `;
