e17160f370bc16944c4f3aa1bd37e4cf7cd47438
Ramon Xavier <ramon1234rxm@gmail.com>
2023-05-29 10:12:21 -0300

    feat: update main app

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+import { Button } from '../components/Button';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
 import { Menu } from '../components/Menu';
+import { TableModal } from '../components/TableModal';
 
 import * as S from './styles';
 
 export const Main = () => {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState('');
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <S.Container>
@@ -18,7 +28,18 @@ export const Main = () => {
         </S.MenuContainer>
       </S.Container>
       <S.Footer>
-        <S.FooterContainer></S.FooterContainer>
+        <S.FooterContainer>
+          {selectedTable.length === 0 && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
+            Novo Pedido
+            </Button>
+          )}
+        </S.FooterContainer>
+        <TableModal
+          visible={isTableModalVisible}
+          onClose={() => setIsTableModalVisible(false)}
+          onSave={handleSaveTable}
+        />
       </S.Footer>
     </>
   );
